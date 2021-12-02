8b2f8946ad5fa50d5f4b03d7e7a7a8b6df3bef19
user <user@GRIGORYWORKPC>
2021-12-02 16:24:35 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+	int sec;
+	cout << "Enter the time interval in seconds: ";
+	cin >> sec;
+	for (int i = 0; i < sec; i++)
+	{
+		Sleep(1000);
+	}
+	cout << "\a";
+}
